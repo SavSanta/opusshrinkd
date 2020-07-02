268b81a086193387a23daf1f3af5f66143968d69
Ru Uba <remethyst@yahoo.com>
2020-07-02 19:56:22 -0400

    beautify C code using onlinegdb.com and 1 manual indent in geany

--- opusshrinkd.c
+++ opusshrinkd.c
@@ -21,140 +21,144 @@
 #define NORMAL_COLOR  "\x1B[0m"
 #define GREEN  "\x1B[32m"
 #define RED  "\x1B[33m"
-#define MAXFILES 3000            // decrease from 10000 to stop SIGSEVs
+#define MAXFILES 3000		// decrease from 10000 to stop SIGSEVs
 #define FILELEN 1000
 
 
-static void opus_shrink_daemon()
-{
-    
-    /* Process below will cause the fork off into a daemon */
-    pid_t pid;
-
-    /* Fork off the parent process */
-    pid = fork();
-    /* If pid is less than zero after fork, then an error occurred. Exit */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
-
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
-    /* Fork off for the second time*/
-    pid = fork();
-
-    /* An error occurred if pid is less than zero */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* Set new file permissions to 755*/
-    umask(022);
-
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/root/opusvoicecalls");
-
-    /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    static void
+    opus_shrink_daemon ()
     {
-        close (x);
-    }
 
-    /* Open the log file */
-    openlog ("opusshrink", LOG_PID, LOG_DAEMON);
-}
+      /* Process below will cause the fork off into a daemon */
+      pid_t pid;
 
-int main()
-{
-    opus_shrink_daemon();
+      /* Fork off the parent process */
+      pid = fork ();
+      /* If pid is less than zero after fork, then an error occurred. Exit */
+      if (pid < 0)
+        exit (EXIT_FAILURE);
+      /* Success: Let the parent terminate */
+      if (pid > 0)
+        exit (EXIT_SUCCESS);
+      /* On success: The child process becomes session leader */
+      if (setsid () < 0)
+        exit (EXIT_FAILURE);
 
-    while (1)
-    {
-        //TODO: Insert daemon code here.
-        syslog (LOG_NOTICE, "Opus Shrink has started.");
-        updatefiles();
-        sleep (10);
-        break;
-    }
+      /* Catch, ignore and handle signals */
+      //TODO: Implement a working signal handler */
+      signal (SIGCHLD, SIG_IGN);
+      signal (SIGHUP, SIG_IGN);
 
-    syslog (LOG_NOTICE, "Opus Shrink has terminated.");
-    closelog();
+      /* Fork off for the second time */
+      pid = fork ();
 
-    return EXIT_SUCCESS;
-}
+      /* An error occurred if pid is less than zero */
+      if (pid < 0)
+        exit (EXIT_FAILURE);
 
+      /* Success: Let the parent terminate */
+      if (pid > 0)
+        exit (EXIT_SUCCESS);
 
-int updatefiles(void)
-{
-     /* Standard declarations */
-    DIR *d;
-    struct dirent *dir;
-    
-    /* declare and initialize a counter variable for files in directory */
-    int count = 0;
+      /* Set new file permissions to 755 */
+      umask (022);
 
-    /* Create a 200 file character ptr array to hold strings of 128 chars max each */
-    char * filelist[MAXFILES][128] = {'\0'};
-    
-    /* File path  */
-    char path[FILELEN]="/root/voicecalls";
-    
-    /* Debug Testing Omit This code in production
-    printf("\nEnter the directory you want to list out!");
-    scanf("%s", &path);
-    */
-    
-    /* Set the path of the d variable */
-    d = opendir(path);
-    
-    /* Create a full path variable */
-    char full_path[FILELEN];
-    
-    // Check if value isnt a negative..ie a fail
-    if (d)
-    {
-        // call readdir to read the next file in the dirent directory and assign it to dir.
-        while ((dir = readdir(d)) != NULL) 
+      /* Change the working directory to the root directory */
+      /* or another appropriated directory */
+      chdir ("/root/opusvoicecalls");
+
+      /* Close all open file descriptors */
+      int x;
+      for (x = sysconf (_SC_OPEN_MAX); x >= 0; x--)
         {
-            //Check if dirent is valued as a regular file. Only expected to work on unix per https://linux.die.net/man/3/readdir
-            if (dir->d_type==DT_REG ) {
-            
-                // following sections builds the full path to the files in the location incrementally using `strcat`
-                full_path[0]='\0';
-                strcat(full_path, path);
-                strcat(full_path, "/");
-                strcat(full_path, dir->d_name);
-                
-                //Copy the full path to the filelist array
-                strcpy(filelist[count], full_path);
-                //printf("%s\n", filelist[count]);
-                
-                // increase counter variable
-                count++;
+          close (x);
+        }
+
+      /* Open the log file */
+      openlog ("opusshrink", LOG_PID, LOG_DAEMON);
+    }
+
+    int
+    main ()
+    {
+      opus_shrink_daemon ();
+
+      while (1)
+        {
+          //TODO: Insert daemon code here.
+          syslog (LOG_NOTICE, "Opus Shrink has started.");
+          updatefiles ();
+          sleep (10);
+          break;
+        }
+
+      syslog (LOG_NOTICE, "Opus Shrink has terminated.");
+      closelog ();
+
+      return EXIT_SUCCESS;
+    }
+
+
+    int
+    updatefiles (void)
+    {
+      /* Standard declarations */
+      DIR *d;
+      struct dirent *dir;
+
+      /* declare and initialize a counter variable for files in directory */
+      int count = 0;
+
+      /* Create a 200 file character ptr array to hold strings of 128 chars max each */
+      char *filelist[MAXFILES][128] = { '\0' };
+
+      /* File path  */
+      char path[FILELEN] = "/root/voicecalls";
+
+      /* Debug Testing Omit This code in production
+         printf("\nEnter the directory you want to list out!");
+         scanf("%s", &path);
+       */
+
+      /* Set the path of the d variable */
+      d = opendir (path);
+
+      /* Create a full path variable */
+      char full_path[FILELEN];
+
+      // Check if value isnt a negative..ie a fail
+      if (d)
+        {
+          // call readdir to read the next file in the dirent directory and assign it to dir.
+          while ((dir = readdir (d)) != NULL)
+        {
+          //Check if dirent is valued as a regular file. Only expected to work on unix per https://linux.die.net/man/3/readdir
+          if (dir->d_type == DT_REG)
+            {
+
+              // following sections builds the full path to the files in the location incrementally using `strcat`
+              full_path[0] = '\0';
+              strcat (full_path, path);
+              strcat (full_path, "/");
+              strcat (full_path, dir->d_name);
+
+              //Copy the full path to the filelist array
+              strcpy (filelist[count], full_path);
+              //printf("%s\n", filelist[count]);
+
+              count++;
             }
         }
-        closedir(d);
+          closedir (d);
+        }
+      else
+        {
+          printf ("Error occured opening the directory! Exiting");
+          syslog (LOG_PERROR,
+              "Error occured opening the directory! Exiting with failure");
+          exit (EXIT_FAILURE);
+
+        }
+      return (0);
+
     }
-    else
-    {
-        printf("Error occured opening the directory! Exiting");
-        syslog(LOG_PERROR, "Error occured opening the directory! Exiting with failure");
-        exit(EXIT_FAILURE);
-        
-    }
-    return(0);   
-    
-}
